9a759ab28965b231d0e2caf5b5837e3126a4c877
Amari Pascoe <elliotpascoe04@gmail.com>
2023-06-11 21:41:30 -0400

    finished recent orders section

---
+++ data/data.js
@@ -0,0 +1,121 @@
+export const data = [
+	{
+		id: 1,
+		name: {
+			first: 'John',
+			last: 'Smith',
+		},
+		total: 2795.95,
+		status: 'On Hold',
+		method: 'PayPal',
+		date: '15 Minutes ago',
+	},
+
+	{
+		id: 2,
+		name: {
+			first: 'Jane',
+			last: 'Doe',
+		},
+		total: 1050.5,
+		status: 'Processing',
+		method: 'Credit Card',
+		date: '2 Hours ago',
+	},
+
+	{
+		id: 3,
+		name: {
+			first: 'Michael',
+			last: 'Johnson',
+		},
+		total: 499.99,
+		status: 'Completed',
+		method: 'Bank Transfer',
+		date: '1 Day ago',
+	},
+
+	{
+		id: 4,
+		name: {
+			first: 'Emily',
+			last: 'Anderson',
+		},
+		total: 75.0,
+		status: 'Cancelled',
+		method: 'PayPal',
+		date: '1 Week ago',
+	},
+
+	{
+		id: 5,
+		name: {
+			first: 'David',
+			last: 'Brown',
+		},
+		total: 199.95,
+		status: 'On Hold',
+		method: 'Credit Card',
+		date: '1 Hour ago',
+	},
+
+	{
+		id: 6,
+		name: {
+			first: 'Sarah',
+			last: 'Taylor',
+		},
+		total: 899.0,
+		status: 'Completed',
+		method: 'Bank Transfer',
+		date: '3 Days ago',
+	},
+
+	{
+		id: 7,
+		name: {
+			first: 'Robert',
+			last: 'Wilson',
+		},
+		total: 499.5,
+		status: 'Processing',
+		method: 'PayPal',
+		date: '6 Hours ago',
+	},
+
+	{
+		id: 8,
+		name: {
+			first: 'Olivia',
+			last: 'Moore',
+		},
+		total: 149.99,
+		status: 'Completed',
+		method: 'Credit Card',
+		date: '1 Day ago',
+	},
+
+	{
+		id: 9,
+		name: {
+			first: 'Daniel',
+			last: 'Clark',
+		},
+		total: 59.95,
+		status: 'Cancelled',
+		method: 'PayPal',
+		date: '2 Weeks ago',
+	},
+
+	{
+		id: 10,
+		name: {
+			first: 'Sophia',
+			last: 'Harris',
+		},
+		total: 299.0,
+		status: 'Processing',
+		method: 'Bank Transfer',
+		date: '2 Hours ago',
+	},
+];
