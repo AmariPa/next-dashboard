1d88841ab09f2d6503e1315c232e3c80d355ee1b
Amari Pascoe <elliotpascoe04@gmail.com>
2023-06-11 20:03:01 -0400

    finished frontend for side and top bars

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Cards from '../components/Cards';
 
 export default function Home() {
 	return (
@@ -11,6 +12,7 @@ export default function Home() {
 			</Head>
 			<main className='bg-gray-100 min-h-screen'>
 				<Header />
+				<Cards />
 			</main>
 		</>
 	);
